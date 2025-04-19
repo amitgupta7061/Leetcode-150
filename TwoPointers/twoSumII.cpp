0da6c77ffd7f4654e5b78146e9c35c0edd4d3c95
Amit <amitgupta561710@gmai.com>
2025-04-19 12:17:05 +0530

    Two-pointers

---
+++ TwoPointers/twoSumII.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = numbers.size() - 1;
+        while(i < j){
+            if(numbers[i] + numbers[j] > target) j--;
+            else if(numbers[i] + numbers[j] < target) i++;
+            else return {i+1, j+1};
+        }
+        return {-1, -1};
+    }
+};
+
+int main() {
+
+    return 0;
+}
