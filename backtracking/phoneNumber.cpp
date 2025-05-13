8d0b00a19274b2eaa68e028844b890f0fd63a675
Amit <amitgupta561710@gmai.com>
2025-05-13 16:23:40 +0530

    backtracking

--- backtracking/phoneNumber.cpp
+++ backtracking/phoneNumber.cpp
@@ -3,8 +3,25 @@ using namespace std;
 
 class Solution {
 public:
+    void f(string &digits, int ind, string &str, vector<string> &result, vector<string> &map){
+        if(ind == digits.length()){
+            result.push_back(str);
+            return;
+        }
+        int index = digits[ind] - '0';
+        string s = map[index];
+        for(int i = 0; i < s.size(); i++){
+            str.push_back(s[i]);
+            f(digits, ind+1, str,result,map);
+            str.pop_back();
+        }
+    }
     vector<string> letterCombinations(string digits) {
-        
+        vector<string> map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> result;
+        string str = "";
+        f(digits, 0, str, result, map);
+        return result;
     }
 };
 
